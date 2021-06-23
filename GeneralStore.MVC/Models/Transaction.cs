566b3b5cdbb913e346593263a286d2d93252435a
Frederick McClung <Frederick.McClung.SWDev@gmail.com>
2021-06-23 03:06:22 -0400

    Created Transaction model, controller & index view

---
+++ GeneralStore.MVC/Models/Transaction.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace GeneralStore.MVC.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public int TransactionID { get; set; }
+
+        [Required]
+        [Display(Name = "Product ID")]
+        public int ProductID { get; set; }
+
+        [ForeignKey(nameof(ProductID))]
+        public virtual Product Product { get; set; }
+
+        [Required]
+        [Display(Name = "Customer ID")]
+        public int CustomerID { get; set; }
+
+        [ForeignKey(nameof(CustomerID))]
+        public virtual Customer Customer { get; set; }
+
+        [Required]
+        [Display(Name = "Qty Bought")]
+        public int PurchaseQuantity { get; set; }
+
+        [DataType(DataType.DateTime)]
+        [Display(Name = "Date Purchased")]
+        public DateTime DateOfTransaction { get; set; }
+    }
+}
